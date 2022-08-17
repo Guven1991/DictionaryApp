d016a49691a7f1e530257ba5aebbb69e98a87290
Guven1991 <gayvazoglu.29@gmail.com>
2022-08-17 17:25:48 +0300

    english to turkish added

--- src/main/java/com/example/dictionaryapp/controller/WordController.java
+++ src/main/java/com/example/dictionaryapp/controller/WordController.java
@@ -16,6 +16,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 @RestController
 @RequestMapping
@@ -37,8 +39,8 @@ public class WordController {
                 Word word = new Word();
                 Row row = worksheet.getRow(i);
                 word.setId((long) row.getCell(0).getNumericCellValue());
-                word.setEnglish(row.getCell(1).getStringCellValue());
-                word.setTurkish(row.getCell(2).getStringCellValue());
+                word.setEnglish(row.getCell(1).getStringCellValue().toUpperCase(Locale.ROOT).trim());
+                word.setTurkish(row.getCell(2).getStringCellValue().toUpperCase().trim());
                 word.setCorrectNumber((int) row.getCell(3).getNumericCellValue());
                 wordList.add(word);
                 wordRepository.save(word);
@@ -49,25 +51,40 @@ public class WordController {
     }
 
     @GetMapping("all-word")
-    public ResponseEntity<List<Word>> getAllWord(){
-       return ResponseEntity.ok(wordRepository.findAll());
+    public ResponseEntity<List<Word>> getAllWord() {
+        return ResponseEntity.ok(wordRepository.findAll());
     }
 
     @GetMapping("turkish/{id}")
-    public ResponseEntity<String> turkishWordById(@PathVariable Long id){
+    public ResponseEntity<String> turkishWordById(@PathVariable Long id) {
 
         Word word = wordRepository.findById(id).orElse(null);
 
 
         return ResponseEntity.ok(word.getTurkish());
     }
+
     @GetMapping("english/{id}")
-    public ResponseEntity<String> englishWordById(@PathVariable Long id){
+    public ResponseEntity<String> englishWordById(@PathVariable Long id) {
 
         Word word = wordRepository.findById(id).orElse(null);
 
 
         return ResponseEntity.ok(word.getEnglish());
+    }
+
+    @GetMapping("englishtoturkish/{englishWord}")
+    public ResponseEntity<String> englishToTurkish(@PathVariable String englishWord) {
+        String bulunanWord = null;
+        List<Word> wordList = wordRepository.findAll();
+
+        for (Word word : wordList) {
+            if ((Objects.equals(englishWord, word.getEnglish()))) {
+                bulunanWord = word.getTurkish();
+                System.out.println(bulunanWord);
+            }
+        }
+            return ResponseEntity.ok(bulunanWord);
     }
 
 
